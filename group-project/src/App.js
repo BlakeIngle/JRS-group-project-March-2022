44ecfa8d4d283946abd5ccf6cf073eaea042a4e0
maygenlee <1maygenl@gmail.com>
2022-02-01 12:28:59 -0500

    set up react-router

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<p>Home page goes here</p>}></Route>
+            {/* <Route path="/login" element={<LoginPage />}></Route> */}
+            {/* <Route path="/signup" element={<SignUpPage />}></Route> */}
+            {/* <Route path="/user/:userId" element={<UserPage />}></Route> */}
+            <Route path="*" element={<div>404 - page does not exist</div>}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
