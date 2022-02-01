a7375f6c604401c9a55b939e3208da8386c7c4e7
monaghanhc <56236377+monaghanhc@users.noreply.github.com>
2022-02-01 15:03:15 -0500

    hooked up router for sign up page

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
           <Routes>
             <Route path="/" element={<p>Home page goes here</p>}></Route>
             <Route path="/login" element={<LoginPage />}></Route>
-            {/* <Route path="/signup" element={<SignUpPage />}></Route> */}
+            <Route path="/signup" element={<SignUpPage />}></Route>
             {/* <Route path="/user/:userId" element={<UserPage />}></Route> */}
             <Route path="*" element={<div>404 - page does not exist</div>}></Route>
           </Routes>
