4e1f151460ab44acfc6a722d23ac19bc33aac086
maygenlee <1maygenl@gmail.com>
2022-02-07 09:17:29 -0500

    connected yelp api to our api

--- server/app/controllers/dish.controller.js
+++ server/app/controllers/dish.controller.js
@@ -1,184 +1,183 @@
-const db = require('../index');
+const db = require("../index");
+const yelp = require("./yelp.controller");
 
 exports.getAllDishes = (req, res) => {
-    // contact sql, get all (*) -> send data back to client
-    const query = "SELECT * FROM dishes.dish;";
+  // contact sql, get all (*) -> send data back to client
+  const query = "SELECT * FROM dishes.dish;";
 
-    db.query(query, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error retrieving dishes." })
-        } else {
-            if (results.length == 0) {
-                res.status(404)
-                    .send({ message: "No dishes found." });
-            } else {
-                res.send({ dishes: results });
-            }
-        }
-    });
-}
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error retrieving dishes." });
+    } else {
+      if (results.length == 0) {
+        res.status(404).send({ message: "No dishes found." });
+      } else {
+        res.send({ dishes: results });
+      }
+    }
+  });
+};
 
 exports.getDishById = (req, res) => {
+  const { dishId } = req.params;
 
-    const { dishId } = req.params;
+  const query = `SELECT * FROM dishes WHERE id = ?;`;
+  const placeholders = [dishId];
 
-    const query = `SELECT * FROM dishes WHERE id = ?;`;
-    const placeholders = [dishId];
-
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error retrieving dish." })
-        } else {
-            if (results.length == 0) {
-                res.status(404)
-                    .send({ message: "No dish found." });
-            } else {
-                res.send({ dish: results[0] });
-            }
-        }
-    });
-}
+  db.query(query, placeholders, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error retrieving dish." });
+    } else {
+      if (results.length == 0) {
+        res.status(404).send({ message: "No dish found." });
+      } else {
+        res.send({ dish: results[0] });
+      }
+    }
+  });
+};
 
 exports.getDishByLocation = (req, res) => {
+  const { location } = req.params;
 
-    const { location } = req.params;
-
-    const query = `SELECT * FROM dishes.dish WHERE location = ?;`;
-    const placeholders = [location];
-
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error retrieving dish." })
-        } else {
-            if (results.length == 0) {
-                res.status(404)
-                    .send({ message: "No dish found." });
-            } else {
-                res.send({ dish: results[0] });
-            }
-        }
+  yelp
+    .getRestaurantsNearZipCode(location)
+    .then((result) => {
+      console.log(result.data.businesses);
+      // send request to our DB
+      res.send(result.data.businesses);
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+      res.status(500).send({
+        error: error,
+        message: "There was a problem getting restaurants",
+      });
     });
-}
+};
+
+exports.getRestaurant = (req, res) => {
+  const { name, location } = req.params;
+
+  yelp
+    .searchRestaurantsByName(name, location)
+    .then((result) => {
+      console.log(result.data.businesses);
+      res.send(result.data.businesses);
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+      res.status(500).send({
+        error: error,
+        message: "There was a problem getting restaurants",
+      });
+    });
+};
 
 exports.getDishesByQuery = (req, res) => {
-
-    console.log("recieved request")
-
-    var query = `SELECT * FROM dishes.dish 
+  var query = `SELECT * FROM dishes.dish 
                     WHERE `;
 
-    var isAndAdded = false;
-    var placeholders = [];
+  var isAndAdded = false;
+  var placeholders = [];
 
-    for (let prop in req.query) {
-
-        if (isAndAdded) {
-            query += 'AND '
-        }
-        isAndAdded = true;
-        query += `${prop} LIKE ? `
-        placeholders.push(`%${req.query[prop]}%`);
+  for (let prop in req.query) {
+    if (isAndAdded) {
+      query += "AND ";
     }
-    query += ';';
+    isAndAdded = true;
+    query += `${prop} LIKE ? `;
+    placeholders.push(`%${req.query[prop]}%`);
+  }
+  query += ";";
 
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error retrieving dishes." })
-        } else {
-            if (results.length == 0) {
-                res.status(404)
-                    .send({ message: "No dishes found." });
-            } else {
-                res.send({ dishes: results });
-            }
-        }
-    });
-}
+  db.query(query, placeholders, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error retrieving dishes." });
+    } else {
+      if (results.length == 0) {
+        res.status(404).send({ message: "No dishes found." });
+      } else {
+        res.send({ dishes: results });
+      }
+    }
+  });
+};
 
 exports.addNewDish = (req, res) => {
+  const pairs = Object.entries(req.body);
 
-    const pairs = Object.entries(req.body);
+  var columns = ""; // `id, name, location`
+  var values = ""; // `123, 'Pizza', 'Charleston'`
+  const placeholders = [];
 
-    var columns = ''; // `id, name, location`
-    var values = ''; // `123, 'Pizza', 'Charleston'`
-    const placeholders = [];
+  for (let i = 0; i < pairs.length; i++) {
+    let [key, value] = pairs[i];
+    columns += (i != 0 ? ", " : "") + "??";
+    placeholders.push(key);
+  }
 
-    for (let i = 0; i < pairs.length; i++) {
-        let [key, value] = pairs[i];
-        columns += (i != 0 ? ', ' : '') + '??'
-        placeholders.push(key);
-    }
+  for (let i = 0; i < pairs.length; i++) {
+    let [key, value] = pairs[i];
+    values += (i != 0 ? ", " : "") + "?";
+    placeholders.push(value);
+  }
 
-    for (let i = 0; i < pairs.length; i++) {
-        let [key, value] = pairs[i];
-        values += (i != 0 ? ', ' : '') + '?'
-        placeholders.push(value);
-    }
-
-    const query = `INSERT INTO dishes.dish (${columns})
+  const query = `INSERT INTO dishes.dish (${columns})
                     values (${values});`;
 
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error posting dish." })
-        } else {
-            res.send({
-                message: "Dish posted successfully.",
-                results: results
-            });
-        }
-    });
-}
+  db.query(query, placeholders, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error posting dish." });
+    } else {
+      res.send({
+        message: "Dish posted successfully.",
+        results: results,
+      });
+    }
+  });
+};
 
 exports.updateDish = (req, res) => {
+  var { name, location, id } = req.body;
 
-    var { name, location, id } = req.body;
-
-    // send the correct SQL script to the DB
-    var query = `UPDATE dishes.dish
+  // send the correct SQL script to the DB
+  var query = `UPDATE dishes.dish
                     SET 
                         name = ? ,
                         location = ? ,
-                    WHERE id = ? ;`
+                    WHERE id = ? ;`;
 
-    var placeholders = [name, location, id];
+  var placeholders = [name, location, id];
 
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error updating dish." })
-        } else {
-            res.send({
-                message: "Dish updated successfully.",
-                results: results
-            });
-        }
-    });
-}
+  db.query(query, placeholders, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error updating dish." });
+    } else {
+      res.send({
+        message: "Dish updated successfully.",
+        results: results,
+      });
+    }
+  });
+};
 
 exports.deleteDishById = (req, res) => {
+  var { dishId } = req.params;
 
-    var { dishId } = req.params;
+  var query = `DELETE FROM dishes.dish
+	                WHERE id = ? ;`;
 
-    var query = `DELETE FROM dishes.dish
-	                WHERE id = ? ;`
+  var placeholders = [dishId];
 
-    var placeholders = [dishId];
-
-    db.query(query, placeholders, (err, results) => {
-        if (err) {
-            res.status(500)
-                .send({ error: err, message: "Error deleting dish." })
-        } else {
-            res.send({
-                message: "Dish deleted successfully.",
-                results: results
-            });
-        }
-    });
-}
+  db.query(query, placeholders, (err, results) => {
+    if (err) {
+      res.status(500).send({ error: err, message: "Error deleting dish." });
+    } else {
+      res.send({
+        message: "Dish deleted successfully.",
+        results: results,
+      });
+    }
+  });
+};
