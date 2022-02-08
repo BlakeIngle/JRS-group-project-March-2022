29b5d48edac20b920acc2f91110f209dd88cdea0
troyahudson <94701592+troyahudson@users.noreply.github.com>
2022-02-08 09:02:37 -0500

    added DishPage functionality to display all dishes

--- group-project/src/components/DishPage/DishPage.js
+++ group-project/src/components/DishPage/DishPage.js
@@ -1,35 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Emojis } from '../../assets/DishIcon';
 import { useApi } from '../../services/axios.services';
+import '../DishPage/DishPage.css'
 
 
 export default function DishPage({ dishId, name, meal, cuisine }) {
 
-    
+    const [dishes, setDishes] = useState([]);
+
     const api = useApi();
 
     function getDishes() {
         api.getAllDishes()
-        .then(res => {
-           console.log(res);
-        })
-        .catch(err => {
-            console.error(err)
-        })
+            .then(res => {
+                setDishes(res.data.dishes)
+            })
+            .catch(err => {
+                console.error(err)
+            })
     };
 
     useEffect(() => {
         getDishes();
+        // console.log(dishes);
     }, []);
-    
-        // const dishId = 1;
-    // const dishIcon = dishIcons[(dishId - 1)].emoji;
+
+
+    function DishItem({ id, name, meal, cuisine }) {
+        return (
+            <div className='dish-item'>{Emojis[name]} {name}</div>
+        )
+    }
 
 
     return (
         <div className='dish-page-root'>
-            <h2>Restaurant</h2>
+            <div className='dishes-list'>
+                    {dishes.map(dish => {
+                        return (
+                            <DishItem key={dish.id}
+                                {...dish}
+                            />
+                        )
+                    })}
+            </div>
             {/* RestaurantCards go here */}
         </div>
     );
+
 }
